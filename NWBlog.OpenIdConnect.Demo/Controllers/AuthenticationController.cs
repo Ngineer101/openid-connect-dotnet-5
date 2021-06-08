b68411b84bc4fb318a792d6020b619df7d584041
Ngineer101 <nicolaasb@pm.me>
2021-06-08 21:28:15 +0200

    Step 9

---
+++ NWBlog.OpenIdConnect.Demo/Controllers/AuthenticationController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NWBlog.OpenIdConnect.Demo.Identity;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NWBlog.OpenIdConnect.Demo.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthenticationController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("~/connect/token")]
+        [Consumes("application/x-www-form-urlencoded")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Exchange()
+        {
+            var oidcRequest = HttpContext.GetOpenIddictServerRequest();
+            if (oidcRequest.IsPasswordGrantType())
+                return await TokensForPasswordGrantType(oidcRequest);
+
+            if (oidcRequest.IsRefreshTokenGrantType())
+            {
+                // return tokens for refresh token flow
+            }
+
+            if (oidcRequest.GrantType == "custom_flow_name")
+            {
+                // return tokens for custom flow
+            }
+
+            return BadRequest(new OpenIddictResponse
+            {
+                Error = OpenIddictConstants.Errors.UnsupportedGrantType
+            });
+        }
+
+        private async Task<IActionResult> TokensForPasswordGrantType(OpenIddictRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.Username);
+            if (user == null)
+                return Unauthorized();
+
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (signInResult.Succeeded)
+            {
+                var identity = new ClaimsIdentity(
+                    TokenValidationParameters.DefaultAuthenticationType,
+                    OpenIddictConstants.Claims.Name,
+                    OpenIddictConstants.Claims.Role);
+
+                identity.AddClaim(OpenIddictConstants.Claims.Subject, user.Id.ToString(), OpenIddictConstants.Destinations.AccessToken);
+                identity.AddClaim(OpenIddictConstants.Claims.Username, user.Username, OpenIddictConstants.Destinations.AccessToken);
+                // Add more claims if necessary
+
+                foreach (var userRole in user.UserRoles)
+                {
+                    identity.AddClaim(OpenIddictConstants.Claims.Role, userRole.Role.NormalizedName, OpenIddictConstants.Destinations.AccessToken);
+                }
+
+                var claimsPrincipal = new ClaimsPrincipal(identity);
+                claimsPrincipal.SetScopes(new string[]
+                {
+                    OpenIddictConstants.Scopes.Roles,
+                    OpenIddictConstants.Scopes.OfflineAccess,
+                    OpenIddictConstants.Scopes.Email,
+                    OpenIddictConstants.Scopes.Profile,
+                });
+
+                return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            }
+            else
+                return Unauthorized();
+        }
+    }
+}
---
+++ NWBlog.OpenIdConnect.Demo/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Validation.AspNetCore;
+
+namespace NWBlog.OpenIdConnect.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// Test endpoint
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+        public IActionResult Test()
+        {
+            var identity = HttpContext.User.Identity;
+            return Ok(new { identity.Name });
+        }
+    }
+}
